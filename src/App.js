08d0e1b23a9b489e735c1a491a90edc25b766662
ACRZeuss <69459810+ACRZeuss@users.noreply.github.com>
2022-04-26 19:38:08 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { DATA } from "./components/Data";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState(null);
+
+  const changeUrl = (url) => {
+    setUrl(url);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <div class="header">
+        <a href="#default" class="logo">
+          Pocket Radio Player
         </a>
-      </header>
+        <div class="header-right">
+          <a id="desktop-app" href="./assets/pocket-radio-player-setup.exe" download>Masaüstü Uygulamasını İndir</a>
+        </div>
+      </div>
+      <div className="App">
+        {DATA.map((item) => {
+          return (
+            <a onClick={() => setUrl(item.url)} className="radio-station">
+              <img src={item.image} />
+            </a>
+          );
+        })}
+      </div>
+      <div className="player">
+        {DATA.map((item) => {
+          if (item.url === url) {
+            return (
+              <div className="station-name">
+                {/* <h3>Şu an çalan Radyo:</h3>
+                <h1>{item.name}</h1> */}
+                <marquee>Şu an çalan Radyo: {item.name}</marquee>
+              </div>
+            );
+          }
+        })}
+        <AudioPlayer
+          autoPlay
+          preload={"none"}
+          showSkipControls={false}
+          showJumpControls={false}
+          volume={0.3}
+          src={url}
+          onPlay={(e) => console.log("onPlay")}
+          // other props here
+        />
+      </div>
     </div>
   );
 }
