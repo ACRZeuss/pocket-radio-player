f5a48254f748756c7b4da639ad3444f48120b510
ACRZeuss <69459810+ACRZeuss@users.noreply.github.com>
2022-05-04 22:14:48 +0300

    update

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,140 +1,196 @@
 export const DATA = [
-    // Template
-    // {
-    //     id: ,
-    //     name: "",
-    //     url: "",
-    //     image: ""
-    // },
-    {
-      id: 1,
-      name: "Best FM",
-      url: "https://best.canli-radyo.biz/;stream.mp3",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9f/Best_fm_logo.png",
-    },
-    {
-      id: 2,
-      name: "Joy FM",
-      url: "http://playerservices.streamtheworld.com/api/livestream-redirect/JOY_FM_SC",
-      image:
-        "https://mediacdns.karnaval.com/media/station_media/3/logos/meta_image.png?v=310718045926",
-    },
-    {
-      id: 3,
-      name: "JoyTürk FM",
-      url: "https://playerservices.streamtheworld.com/api/livestream-redirect/JOY_TURK2.mp3",
-      image:
-        "https://mediacdns.karnaval.com/media/station_media/4/logos/meta_image.png?v=180520022614",
-    },
-    //   {
-    //     id: 4,
-    //     name: "Kafa Radyo",
-    //     url: "https://live.kafaradyo.com/radyoland/kafaradyo.stream/playlist.m3u8",
-    //     image:
-    //       "https://i1.sndcdn.com/artworks-u1FJHUHAQsV9kudC-Dv1puQ-t500x500.jpg",
-    //   },
-    // {
-    //   id: 5,
-    //   name: "Kral FM",
-    //   url: "https://erhanpolat.tk/radio-stations/kralfm/",
-    //   image: "https://radyodi.com/wp-content/uploads/2019/02/kralfmt.png",
-    // },
-    // {
-    //   id: 6,
-    //   name: "Kral Pop Radyo",
-    //   url: "https://erhanpolat.tk/radio-stations/kralpop/",
-    //   image: "https://cdn1.kralmuzik.com.tr/media/content/19-05/20/kralpop.png",
-    // },
-    {
-      id: 6,
-      name: "Power FM",
-      url: "https://listen.powerapp.com.tr/powerfm/mpeg/icecast.audio",
-      image:
-        "https://cdn.powergroup.com.tr/powerapp/channels/big/powerFM.png?v=626",
-    },
-    {
-      id: 7,
-      name: "PowerTürk FM",
-      url: "https://listen.powerapp.com.tr/powerturk/mpeg/icecast.audio?/;stream.mp3",
-      image:
-        "https://cdn.powergroup.com.tr/powerapp/channels/big/powerTurk.png?v=626",
-    },
-    {
-      id: 8,
-      name: "Radyo 45'lik",
-      url: "https://stream.radyo45lik.com:4545/",
-      image:
-        "https://upload.wikimedia.org/wikipedia/tr/thumb/8/80/Radyo_45%27lik_logosu.png/1200px-Radyo_45%27lik_logosu.png",
-    },
-    // {
-    //   id: 9,
-    //   name: "Show Radyo",
-    //   url: "http://windows.showradyo.com.tr/;stream.mp3",
-    //   image:
-    //     "https://pbs.twimg.com/profile_images/1264909493118394368/t8_Hk20n_400x400.jpg",
-    // },
-    {
-      id: 10,
-      name: "SlowTürk Radyo",
-      url: "https://radyo.duhnet.tv/slowturk",
-      image:
-        "https://mytuner.global.ssl.fastly.net/media/tvos_radios/58aAgh9gEd.png",
-    },
-    {
-      id: 11,
-      name: "Dream Türk",
-      url: "https://radyo.duhnet.tv/dreamturk",
-      image:
-        "https://pbs.twimg.com/profile_images/935862506987380737/x8wLxkUx_400x400.jpg",
-    },
-    {
-      id: 12,
-      name: "Metro FM",
-      url: "http://playerservices.streamtheworld.com/api/livestream-redirect/METRO_FM_SC",
-      image:
-        "https://mediacdns.karnaval.com/media/station_media/2/logos/meta_image.png",
-    },
-    // {
-    //   id: 14,
-    //   name: "Pal Station",
-    //   url: "http://shoutcast.radyogrup.com:1020/;",
-    //   image:
-    //     "https://image.winudf.com/v2/image1/Y29tLnBhbF9zdGF0aW9uLnJhZGlvX3NjcmVlbl81XzE1NDkzNDc5MThfMDQw/screen-5.jpg?fakeurl=1&type=.webp",
-    // },
-    // {
-    //   id: 15,
-    //   name: "Pal Akustik",
-    //   url: "http://shoutcast.radyogrup.com:2030/;",
-    //   image:
-    //     "https://is5-ssl.mzstatic.com/image/thumb/Purple91/v4/70/ee/b6/70eeb6e4-9c7b-27a2-4ff1-8b47807237cf/source/512x512bb.jpg",
-    // },
-    // {
-    //   id: 16,
-    //   name: "Pal Slow",
-    //   url: "http://shoutcast.radyogrup.com:2020/;",
-    //   image:
-    //     "https://play-lh.googleusercontent.com/lYPNMkH0s78_H6Vf7l2X93L4AZ2BS0TwfaalzM1DmJxCOaF_gAk69giWcearVepw8dU",
-    // },
-    // {
-    //   id: 17,
-    //   name: "Pal Nostalji",
-    //   url: "http://shoutcast.radyogrup.com:1010/;",
-    //   image:
-    //     "https://image.winudf.com/v2/image1/Y29tLnBhbG5vc3RhbGppLnJhZGlvX3NjcmVlbl8wXzE1NDg2MDc4NTlfMDA3/screen-0.jpg?h=500&fakeurl=1&type=.jpg",
-    // },
-    {
-      id: 13,
-      name: "Virgin Radio",
-      url: "http://playerservices.streamtheworld.com/api/livestream-redirect/VIRGIN_RADIO_SC",
-      image:
-        "https://pbs.twimg.com/profile_images/1125687465266831362/9DtDhuas_400x400.png",
-    },
-    {
-      id: 14,
-      name: "Alem FM",
-      url: "https://turkmedya.radyotvonline.net/alemfmaac",
-      image:
-        "https://pbs.twimg.com/profile_images/1264907825375055872/0171EuGN_400x400.jpg",
-    },
-  ];
+  // Template
+  // {
+  //     id: ,
+  //     name: "",
+  //     url: "",
+  //     image: ""
+  // },
+  {
+    id: 1,
+    name: "Best FM",
+    url: "https://best.canli-radyo.biz/;stream.mp3",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/9f/Best_fm_logo.png",
+  },
+  {
+    id: 2,
+    name: "Joy FM",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/JOY_FM_SC",
+    image:
+      "https://mediacdns.karnaval.com/media/station_media/3/logos/meta_image.png?v=310718045926",
+  },
+  {
+    id: 3,
+    name: "JoyTürk",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/JOY_TURK2.mp3",
+    image:
+      "https://mediacdns.karnaval.com/media/station_media/4/logos/meta_image.png?v=180520022614",
+  },
+  {
+    id: 4,
+    name: "JoyTürk Akustik",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/SC004_SO1_SC?/;stream.mp3",
+    image:
+      "https://mediacdns.karnaval.com/media/station_media/9/logos/android_default_logo.png?v=071118034036",
+  },
+  {
+    id: 5,
+    name: "JoyTürk Rock",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/JOYTURK_ROCK_SC?/;stream.mp3",
+    image:
+      "https://mediacdns.karnaval.com/media/station_media/22/logos/meta_image.png?v=071118024341",
+  },
+  //   {
+  //     id: 4,
+  //     name: "Kafa Radyo",
+  //     url: "https://live.kafaradyo.com/radyoland/kafaradyo.stream/playlist.m3u8",
+  //     image:
+  //       "https://i1.sndcdn.com/artworks-u1FJHUHAQsV9kudC-Dv1puQ-t500x500.jpg",
+  //   },
+  {
+    id: 6,
+    name: "Power FM",
+    url: "https://listen.powerapp.com.tr/powerfm/mpeg/icecast.audio",
+    image:
+      "https://cdn.powergroup.com.tr/powerapp/channels/big/powerFM.png?v=626",
+  },
+  {
+    id: 7,
+    name: "PowerTürk FM",
+    url: "https://listen.powerapp.com.tr/powerturk/mpeg/icecast.audio?/;stream.mp3",
+    image:
+      "https://cdn.powergroup.com.tr/powerapp/channels/big/powerTurk.png?v=626",
+  },
+  {
+    id: 8,
+    name: "Radyo 45'lik",
+    url: "https://stream.radyo45lik.com:4545/",
+    image:
+      "https://upload.wikimedia.org/wikipedia/tr/thumb/8/80/Radyo_45%27lik_logosu.png/1200px-Radyo_45%27lik_logosu.png",
+  },
+  // {
+  //   id: 9,
+  //   name: "Show Radyo",
+  //   url: "http://windows.showradyo.com.tr/;stream.mp3",
+  //   image:
+  //     "https://pbs.twimg.com/profile_images/1264909493118394368/t8_Hk20n_400x400.jpg",
+  // },
+  {
+    id: 10,
+    name: "SlowTürk Radyo",
+    url: "https://radyo.duhnet.tv/slowturk",
+    image:
+      "https://mytuner.global.ssl.fastly.net/media/tvos_radios/58aAgh9gEd.png",
+  },
+  {
+    id: 11,
+    name: "Dream Türk",
+    url: "https://radyo.duhnet.tv/dreamturk",
+    image:
+      "https://pbs.twimg.com/profile_images/935862506987380737/x8wLxkUx_400x400.jpg",
+  },
+  {
+    id: 12,
+    name: "Metro FM",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/METRO_FM_SC",
+    image:
+      "https://mediacdns.karnaval.com/media/station_media/2/logos/meta_image.png",
+  },
+  // {
+  //   id: 14,
+  //   name: "Pal Station",
+  //   url: "http://shoutcast.radyogrup.com:1020/;",
+  //   image:
+  //     "https://image.winudf.com/v2/image1/Y29tLnBhbF9zdGF0aW9uLnJhZGlvX3NjcmVlbl81XzE1NDkzNDc5MThfMDQw/screen-5.jpg?fakeurl=1&type=.webp",
+  // },
+  // {
+  //   id: 15,
+  //   name: "Pal Akustik",
+  //   url: "http://shoutcast.radyogrup.com:2030/;",
+  //   image:
+  //     "https://is5-ssl.mzstatic.com/image/thumb/Purple91/v4/70/ee/b6/70eeb6e4-9c7b-27a2-4ff1-8b47807237cf/source/512x512bb.jpg",
+  // },
+  // {
+  //   id: 16,
+  //   name: "Pal Slow",
+  //   url: "http://shoutcast.radyogrup.com:2020/;",
+  //   image:
+  //     "https://play-lh.googleusercontent.com/lYPNMkH0s78_H6Vf7l2X93L4AZ2BS0TwfaalzM1DmJxCOaF_gAk69giWcearVepw8dU",
+  // },
+  // {
+  //   id: 17,
+  //   name: "Pal Nostalji",
+  //   url: "http://shoutcast.radyogrup.com:1010/;",
+  //   image:
+  //     "https://image.winudf.com/v2/image1/Y29tLnBhbG5vc3RhbGppLnJhZGlvX3NjcmVlbl8wXzE1NDg2MDc4NTlfMDA3/screen-0.jpg?h=500&fakeurl=1&type=.jpg",
+  // },
+  {
+    id: 13,
+    name: "Virgin Radio Türkiye",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/VIRGIN_RADIO_SC",
+    image:
+      "https://pbs.twimg.com/profile_images/1125687465266831362/9DtDhuas_400x400.png",
+  },
+  {
+    id: 14,
+    name: "Alem FM",
+    url: "https://turkmedya.radyotvonline.net/alemfmaac",
+    image:
+      "https://pbs.twimg.com/profile_images/1264907825375055872/0171EuGN_400x400.jpg",
+  },
+  {
+    id: 15,
+    name: "Süper FM",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/SUPER_FM_SC?/;stream.mp3",
+    image:
+      "https://mediacdns.karnaval.com/media/station_media/1/logos/android_default_logo.png?v=261118102233",
+  },
+  {
+    id: 16,
+    name: "Süper2 FM",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/SUPER2_SC?/;stream.mp3",
+    image:
+      "https://mediacdns.karnaval.com/media/station_media/16/logos/android_default_logo.png?v=011019103100",
+  },
+  {
+    id: 17,
+    name: "Radyo D",
+    url: "https://radyo.duhnet.tv/ak_dtvh_radyod?",
+    image:
+      "https://www.beingcozum.com/wp-content/uploads/2021/01/Radyo-D.png",
+  },
+  {
+    id: 18,
+    name: "Kiss FM",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/KISS_FM_SC?/;stream.mp3",
+    image:
+      "https://mediacdns.karnaval.com/media/station_media/224/logos/android_default_logo.png?v=110620123614",
+  },
+  {
+    id: 19,
+    name: "Radio Mydonose",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/SC010_SO1_SC?/;stream.mp3",
+    image:
+      "https://mediacdns.karnaval.com/media/artist_media/5386/biography_600x600/photo_5386_0.jpg?v=110914102328",
+  },
+  {
+    id: 20,
+    name: "Mydonose Türk",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/SC016_SO1_SC?/;stream.mp3",
+    image:
+      "https://mediacdns.karnaval.com/media/station_media/19/logos/meta_image.png?v=011215010437",
+  },
+  // {
+  //   id: 5,
+  //   name: "Kral FM",
+  //   url: "https://erhanpolat.tk/radio-stations/kralfm/",
+  //   image: "https://radyodi.com/wp-content/uploads/2019/02/kralfmt.png",
+  // },
+  // {
+  //   id: 17,
+  //   name: "Kral Pop Radyo",
+  //   url: "https://dygedge.radyotvonline.net/kralpop/playlist.m3u8",
+  //   image: "https://cdn1.kralmuzik.com.tr/media/content/19-05/20/kralpop.png",
+  // },
+];
